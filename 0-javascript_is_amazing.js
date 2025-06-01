09d8a99ebfab7d5140b6a0b3a560a5df88f5dc5a
Gomolemo Mokonyane <theorancem18@gmail.com>
2025-06-01 22:15:26 +0200

    Added README.md file and modified 0-javascrit_is_amazing.js

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,2 +1,2 @@
-const myVar = "JavaScript is amazing"; // Declare a constant variable
-console.log(myVar); // Print the value of myVar
+const myVar = "JavaScript is amazing";  // Declare a constant variable
+console.log(myVar);  // Print the value of myVar
